e29523fbc07059f598dd9d5f18feaa1a9c4d0088
AHMAD <156594920+ahmadabouhilal93@users.noreply.github.com>
2024-01-15 04:19:15 +0300

    100+ Files

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pc101\desktop\new folder (2)\django_hotel_aut_project2\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
